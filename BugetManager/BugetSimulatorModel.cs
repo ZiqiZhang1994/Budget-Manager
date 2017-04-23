9c6b7d7bed7f7767b86267c8b4b63307421554e1
SiriusYan <ZiqiZhang1994>
2017-04-23 17:54:42 +1000

    BugetSimulatorModel added

---
+++ BugetManager/BugetSimulatorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BugetManager
+{
+	public class BugetSimulatorModel
+	{
+		private float _Buget;
+		private List<float> _CostList;
+		public BugetSimulatorModel()
+		{
+			
+		}
+	}
+}
